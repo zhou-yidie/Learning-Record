787063ece0214370a722d27d15ab53328e424ec0
zhou-yidie <2261328488@qq.com>
2024-11-23 15:26:55 +0800

    g++

---
+++ lanqiaobei001.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+set<int> seen;
+
+int main()
+{
+    seen.insert(1);
+    seen.insert(2);
+    seen.insert(3);
+    seen.insert(5);
+    seen.insert(8);
+
+    set<int>::iterator it=seen.begin();    
+    for(int i=0;i<1000;i++)
+    {
+        int x=*it;
+        seen.insert(3*x+2);
+        seen.insert(5*x+3);
+        seen.insert(8*x+5);
+        it++;
+    }
+    int i=0;
+    for(it=seen.begin();it!=seen.end();it++)
+    {
+        i++;
+        if(i==2020)
+            cout<<*it<<endl;
+    }
+
+    return 0;
+}
